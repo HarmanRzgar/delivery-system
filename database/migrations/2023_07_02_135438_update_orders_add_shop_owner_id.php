a5b8e543c06e71158404660d5d20a3ca0e28d73e
HarmanRzgar <108234752+HarmanRzgar@users.noreply.github.com>
2023-07-03 16:03:37 +0300

    omw to get the shop_owner_id with each order

---
+++ database/migrations/2023_07_02_135438_update_orders_add_shop_owner_id.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('shop_Owner_Id')->nullable();
+
+            $table->foreign('shop_owner_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('set null');
+        });
+
+
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2023_07_02_142709_update_carts.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            $table->unsignedBigInteger('shop_owner_id');
+            $table->foreign('shop_owner_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            //
+        });
+    }
+};
